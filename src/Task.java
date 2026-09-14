8dd9c085b613a38f96172721c1e790d44743e0a7
curciog <curciogdev@gmail.com>
2026-09-14 16:38:57 -0300

    first commit

---
+++ src/Task.java
@@ -0,0 +1,18 @@
+import java.time.LocalDateTime;
+
+public class Task {
+
+    private int id;
+    private String description;
+    private TaskStatus status;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+
+    public Task(int id, String description, TaskStatus status) {
+        this.id = id;
+        this.description = description;
+        this.status = status;
+        this.createdAt = LocalDateTime.now();
+        this.updatedAt = LocalDateTime.now();
+    }
+}
