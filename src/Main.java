cd921c794ca154c78ad833bd0abef5c052494f7f
curciog <curciogdev@gmail.com>
2026-09-15 16:44:48 -0300

    refactor: main receives command-line arguments

--- src/Main.java
+++ src/Main.java
@@ -3,9 +3,10 @@ public class Main {
     public static void main(String[] args) {
 
         TaskRepository repository = new TaskRepository();
-
         TaskService service = new TaskService(repository);
 
-        service.addTask("Study Spring");
+        if (args[0].equals("add")) {
+            service.addTask(args[1]);
+        }
     }
 }
